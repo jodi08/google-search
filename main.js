0048fd6e2a2effdf3be069198de594f84089ba84
Jo Anna Mollman <mommollman@gmail.com>
2021-04-22 19:41:21 -0700

    add instructions for site search

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ const google = 'https://www.google.com/search?q=site%3A+';
 
 function submitted(event) {
   event.preventDefault();
-  const url = google + '+' + q.value;
+  const url = google + '+' + q.value;//add + site + where the '+' is
   const win = window.open(url, '_blank');
   win.focus();
   q.value = ''
 }
 
 form.addEventListener('submit', submitted)
+
+//https://pagedart.com/blog/how-to-add-a-search-bar-in-html/ 
+//instructions to add website search
+//const site = 'pagedart.com';
